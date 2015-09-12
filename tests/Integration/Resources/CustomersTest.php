3f9735e059bc7f2cb4ef79fb720e9b97da16d436
Johnny Green <johnnygreen@gmail.com>
2015-09-11 23:23:09 -0400

    tests back to passing

--- tests/Integration/Resources/CustomersTest.php
+++ tests/Integration/Resources/CustomersTest.php
@@ -10,19 +10,48 @@ class CustomersTest extends \TestCase {
     $this->destructive = false;
   }
 
+  public function testStore()
+  {
+    if ($this->destructive)
+    {
+      $attributes = [
+        'customers_id'            => '1000001',
+        'customers_firstname'     => 'Test',
+        'customers_lastname'      => 'Customer',
+        'customers_telephone'     => '123-123-1234',
+        'customers_email_address' => 'test@mzwallace.com'
+      ];
+
+      $customer = $this->repository->create($attributes);
+      $this->assertInstanceOf($this->repository->model, $customer);
+
+      foreach($attributes as $key => $value)
+      {
+        $this->assertEquals($value, $customer->$key);
+      }
+    }
+    else
+    {
+      $customer = new Customer(['ns_id' => 9279, 'customers_id' => '8672']);
+    }
+
+    return $customer;
+  }
+
   /**
    * @depends testStore
    */
   public function testShow($model)
   {
     // test the show endpoint with internal id
+
     $customer = $this->repository->find($model->ns_id);
-    $this->assertInstanceOf('Johnnygreen\LaravelNetSuite\NetSuite\Customer', $customer);
+    $this->assertInstanceOf($this->repository->model, $customer);
     $this->assertEquals($model->ns_id, $customer->ns_id);
 
     // test the show endpoint with external id
     $customer = $this->repository->findByExternalId(['customers_id' => $model->customers_id]);
-    $this->assertInstanceOf('Johnnygreen\LaravelNetSuite\NetSuite\Customer', $model);
+    $this->assertInstanceOf($this->repository->model, $model);
     $this->assertEquals($model->customers_id, $customer->customers_id);
 
     return $model;
@@ -34,10 +63,41 @@ class CustomersTest extends \TestCase {
   public function testSearch($model)
   {
     $customer = $this->repository->where('externalid', 'is', $model->customers_id)->first();
-    $this->assertInstanceOf('Johnnygreen\LaravelNetSuite\NetSuite\Customer', $customer);
+    $this->assertInstanceOf($this->repository->model, $customer);
     $this->assertEquals($model->customers_id, $customer->customers_id);
 
     return $model;
+  }
+
+  /**
+   * @depends testSearch
+   */
+  public function testUpdate($model)
+  {
+    if ($this->destructive)
+    {
+      $model->customers_firstname = 'New First Name';
+      $customer = $this->repository->update($model);
+      $this->assertInstanceOf($this->repository->model, $customer);
+      $this->assertEquals($model->customers_firstname, $customer->customers_firstname);
+    }
+
+    return $model;
+  }
+
+  /**
+   * @depends testUpdate
+   */
+  public function testDestroy($model)
+  {
+    if ($this->destructive)
+    {
+      $success = $this->repository->destroy($model->ns_id);
+      $this->assertTrue($success);
+
+      $customer = $this->repository->find($model->ns_id);
+      $this->assertNull($customer);
+    }
   }
 
 }
--- tests/Integration/Resources/ProductsTest.php
+++ tests/Integration/Resources/ProductsTest.php
@@ -1,13 +1,36 @@
 <?php namespace Resources;
 
-use Johnnygreen\LaravelNetSuite\NetSuite\Product;
-
-class ProductsTest extends \Test {
+class ProductsTest extends \TestCase {
 
   public function setUp()
   {
-    $this->repository  = \NetSuite::getRepository('Product');
+    $this->repository  = \NetSuite::getRepository('InventoryItem');
     $this->destructive = false;
+  }
+
+  public function testShow()
+  {
+    $products_id = '3114';
+
+    // test the show endpoint with external id
+    $product = $this->repository->findByExternalId(['products_id' => $products_id]);
+    $this->assertInstanceOf($this->repository->model, $product);
+    $this->assertEquals($product->products_id, $product->products_id);
+
+    $ns_id = $product->ns_id;
+
+    // test the show endpoint with internal id
+    $product = $this->repository->find($ns_id);
+    $this->assertInstanceOf($this->repository->model, $product);
+    $this->assertEquals($product->ns_id, $ns_id);
+  }
+
+  public function testSearch()
+  {
+    $products_id = '3114';
+    $product = $this->repository->where('externalid', 'is', $products_id)->first();
+    $this->assertInstanceOf($this->repository->model, $product);
+    $this->assertEquals($product->products_id, $products_id);
   }
 
 }
